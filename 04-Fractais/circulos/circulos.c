08dc4d3c12d291740af56600a2bb03624ad03d6d
DeigelaLima <deigelalima@hotmail.com>
2019-03-31 12:16:21 -0300

    Updated

--- 04-Fractais/circulos/circulos.c
+++ 04-Fractais/circulos/circulos.c
@@ -1,4 +1,5 @@
 #include "xpaint.h"
+#include <stdio.h>
 
 void circulos(int x, int y, int tamRaio){
 
@@ -13,7 +14,7 @@ void circulos(int x, int y, int tamRaio){
         int xf = x + tamRaio * xm_cos(angulo);
         int yf = y - tamRaio * xm_sin(angulo);
         
-        xd_circle(xf, yf, tamRaio/3);
+        xd_circle(xf, yf, tamRaio / 3);
         circulos(xf, yf, tamRaio / 3);
         angulo = angulo + 60;
         x_step("circulos"); //salva o arquivo, mas permite ao usuario controlar
@@ -25,8 +26,10 @@ int main(){
     int largura = 800, altura = 800;
     int x = largura / 2;
     int y = altura / 2;
+    printf("Vamos desenhar\n");
     x_open(largura, altura);
     x_clear(BLACK); //Cor de plano de fundo
+    //printf("Ta limpo");
     circulos(x, y, 250);
     x_save("circulos");//salva um png no arquivo, nao inclui a extensao .png no nome do arquivo
     x_close();//finaliza o bitmap liberando os recursos
