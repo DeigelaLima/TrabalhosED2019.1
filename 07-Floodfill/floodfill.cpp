ff195dcc206c5ff43a30e0a67d904f418df5b310
DeigelaLima <deigelalima@hotmail.com>
2019-04-16 18:11:28 -0300

    Updated

--- 07-Floodfill/floodfill.cpp
+++ 07-Floodfill/floodfill.cpp
@@ -6,7 +6,7 @@
 # include  < iostream >
 using  namespace  std ;
 
-char *create_vet(vector<string> &mat){//matriz para vetor
+/*char *create_vet(vector<string> &mat){//matriz para vetor
     char * vet = new char[mat.tamanho() * mat[0].tamanho()];//criação do vetor dinamico
     //preencher vetor
     for(int l = 0; l < mat.tamanho(); l++){
@@ -15,7 +15,7 @@ char *create_vet(vector<string> &mat){//matriz para vetor
         }
     }
     return vet;
-}
+}*/
 struct  Pos {
     int l, c;
     Pos ( int _l, int _c) {
@@ -40,44 +40,37 @@ void  pintar (vetor <string> &mat, int l, int c, char cor_base, char cor_final) 
     if (! tem_valor (mat, l, c, cor_base))//se não for a cor_base retorne
         return;
     mat [l][c] = cor_final;//pintar a matriz cor_final -> azul
-    mat_show(mat);
-    //xmat_draw (mat);//desenha matriz
+    xmat_draw (mat);//desenha matriz
     x_step ( " mat " );
     for ( auto viz: get_neibs (l, c)) {//para cada um dos vizinho
         pintar (mat, viz.l , viz.c , cor_base, cor_final);
     }
 }
-/*void mat_show(vector<string> &mat){
-    char *vet = create_vet(mat);
-    xmat_draw(mat.tamanho(), mat[0].tamanho(),vet);
-    free(vet);
-
-}*/
 void  mostrar_matriz (vector <vector < int >> & mat_int, vetor <string> & mat, fila <Pos> fila) {
     xmat_draw (mat);
     while (! fila. empty ()) {
         xmat_put_circle (fila. frente (). l , fila. frente (). c , BRANCO);
         fila. pop ();
     }
-    for ( int l = 0 ; l <( int ) mat. tamanho (); l ++) {
-        for ( int c = 0 ; c <( int ) mat [ 0 ]. tamanho (); c ++) {
+    for ( int l = 0 ; l < ( int ) mat. tamanho (); l ++) {
+        for ( int c = 0 ; c < ( int ) mat [ 0 ]. tamanho (); c ++) {
             xmat_put_number (l, c, PRETO, mat_int [l] [c]);
         }
     }
-    x_step ( " mat " );
+    x_step ("mat");
 
 }
-void  floodfill (vetor <vetor < int >> & mat_int, vetor <string> & mat, int l, int c, caractere cor_base, char cor_final) {
+void  floodfill (vector <vector < int >> & mat_int, vector <string> & mat, int l, int c, char cor_base, char cor_final) {
     Fila <Pos> fila;
-    fila. push ( Pos (l, c));
+    fila.push( Pos (l, c));
     mat [l] [c] = cor_final;
     while (! fila. empty ()) {
-        Pos ref = fila. frente ();
-        fila. pop ();
-        for ( auto viz: get_neibs (ref. l , ref. c )) {
+        Pos ref = fila.front();//frente
+        fila.pop();
+        for ( auto viz: get_neibs (ref.l , ref.c )) {
             if ( tem_valor (mat, viz. l , viz. c , cor_base)) {
-                mat [viz. l ] [viz. c ] = cor_final;
-                fila. empurre (viz);
+                mat [viz.l] [viz.c] = cor_final;
+                fila.push(viz);//empurre
                 mostrar_matriz (mat_int, mat, fila);
             }
         }
@@ -89,22 +82,21 @@ int  main () {
     vector <string> mat (nl, string (nc, ' g ' ));//criou a matriz
     //vector <vetor < int >> mat_int (nl, vetor < int > (nc, - 1 ));
     
-    for ( int l = 0 ; l < (int) mat. tamanho (); l ++) {
-        for ( int c = 0 ; c < (int) mat [ 0 ]. tamanho (); c ++) {
+    for ( int l = 0 ; l < (int) mat. tamanho (); l++) {
+        for ( int c = 0 ; c < (int) mat [ 0 ]. tamanho (); c++) {
             if ( xm_rand ( 0 , 100 ) < 30 )
                 mat [l][c] = ' r ' ;
         }
     }
-    //mat_show(mat)
     xmat_draw (mat);
-    x_save ( " mat " );
+    x_save ("mat");
     int l = 0 , c = 0 ;
-    puts ( " Digite o ponto de inicio lec " );
-    scanf ( " % d% d " , & l, & c);
+    puts (" Digite o ponto de inicio l e c ");
+    scanf ("%d %d " , &l, &c);
     getchar (); // remove \ n depois dos números
 
 //     pintar (esteira, l, c, esteira [l] [c], 'b');
-    pintar(mat, l, c, mat [l][c], ' b ' );// cor final da matriz azul
+    floodfill(mat_int, mat, l, c, mat [l][c], ' b ' );// cor final da matriz azul
     xmat_draw (mat);//desenha matriz
     x_save ( " mat " );
     x_close ();
