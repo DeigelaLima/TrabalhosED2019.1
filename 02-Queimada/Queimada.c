7e3d629c16bca0a0a0c9cd4c95158efbc0c5032d
DeigelaLima <deigelalima@hotmail.com>
2019-05-01 20:30:17 -0300

    Updated

--- 02-Queimada/Queimada.c
+++ 02-Queimada/Queimada.c
@@ -3,13 +3,12 @@
 int const TREE = '#';//arvore
 int const FIRE = 'o';//fogo
 int const EMPTY = '.';//vazio
+//int const CAMINHO = 'x';
 
 void mostrar_matriz(int nl, int nc, char mat[nl][nc]){
-	int l = 0;
-	int c = 0;
-    for(l = 0; l < nl; l++){
-      for(c = 0; c < nc; c++)
-        	printf("%c", mat[l][c]);
+    for(int i = 0; i < nl; i++){
+      for(int j = 0; j < nc; j++)
+        	printf("%c", mat[i][j]);
       puts("");
     }
 }
@@ -21,15 +20,18 @@ int tocar_fogo(int nl, int nc, char mat[nl][nc], int l, int c){
         
     mat[l][c] = FIRE;
     mostrar_matriz(nl, nc, mat);
-//    getchar();
+    getchar();
     
     int pegarFogo = 1;
     pegarFogo += tocar_fogo(nl, nc, mat, l, c + 1);
     pegarFogo += tocar_fogo(nl, nc, mat, l, c - 1);
     pegarFogo += tocar_fogo(nl, nc, mat, l - 1, c);
     pegarFogo += tocar_fogo(nl, nc, mat, l + 1, c);
+
+    //mat[l][c] = CAMINHO;
     mostrar_matriz(nl, nc, mat);
-	return pegarFogo;
+    getchar();
+	  return pegarFogo;
 }
 int main(){
     int nl = 0;
@@ -38,16 +40,20 @@ int main(){
     int c = 0;
     int incendio = 0;
     
+    printf("Digite o numero de Linha e Coluna: ");
+    scanf("%d %d", &nl, &nc);
     
-    scanf("%d %d %d %d", &nl, &nc, &l, &c);
+    printf("Digite a posicao da Linha e da Coluna para queimar a arvore: ");
+    scanf("%d %d", &l, &c);
     char mat[nl][nc];
     
-    for(l = 0; l < nl; l++){
-    	for(c = 0; c < nc; c++){
-          scanf(" %c", &mat[l][c]);
+    
+    for(int i = 0; i < nl; i++){
+    	for(int j = 0; j < nc; j++){
+          scanf(" %c", &mat[i][j]);
         }
 	}
-    incendio += tocar_fogo(nl, nc, mat, l, c);
+    incendio = tocar_fogo(nl, nc, mat, l, c);
     mostrar_matriz(nl, nc, mat);
     printf("Total de arvores queimadas:%d\n", incendio);
     return 0;
