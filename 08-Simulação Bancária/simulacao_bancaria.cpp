6262804e4b32f54057ca4d3b7b699d4323952ebc
DeigelaLima <deigelalima@hotmail.com>
2019-06-27 18:27:32 -0300

    Update

--- 08-Simulação Bancária/simulacao_bancaria.cpp
+++ 08-Simulação Bancária/simulacao_bancaria.cpp
@@ -1,7 +1,8 @@
-#include <list>
+#include <iostream>
+#include <sstream>
 #include <vector>
 #include <queue>
-#include <iostream>
+#include <list>
 
 using namespace std;
 
@@ -10,92 +11,128 @@ struct Cliente{
     int docs;
     int paciencia;
 
-    Cliente(String id, int docs, int paciencia){
+    Cliente(string id, int docs, int paciencia){
         this-> id = id;
         this-> docs = docs;
         this-> paciencia = paciencia;
     };
 
 };
+
 struct Banco{
     vector<Cliente*>caixas; // Eh um vector de ponteiro para cliente com nome caixas      (int vetor[10]) 
     list<Cliente*>fila_entrada;
     queue<Cliente*>fila_saida;
 
     void mostrar_vector(){
-        for(int i = 0; i < this-> caixas.size(); i++){
-            if(this-> caixas != nullptr){
-                cout << "[" << this-> caixas-> id << ":" << this-> caixas-> docs ":" << this-> caixas->paciencia << "]";
-            }else{
-                cout << " []" << endl;
+            //MOSTRA O VETOR DE CAIXAS
+            for(int i = 0; i < this-> caixas.size(); i++){
+                if(this-> caixas[i] != NULL){
+                    cout << "[" << this-> caixas[i]-> id << ":" << this-> caixas[i]-> docs << ":" << this-> caixas[i]->paciencia << "]";
+                }else{
+                    cout << "[]";
+                }
             }
-        }
+            cout << endl;
+            //MOSTRA FILA DE ENTRADA
+            cout << "in :{";
+            list<Cliente*>::iterator it;
+            for(it = this->fila_entrada.begin(); it != this->fila_entrada.end(); it++){
+                cout << (*it)->id << ":" <<(*it)->docs << ":" << (*it)-> paciencia << " ";
+            }
+            cout << "}" << endl;
+    
+            //MOSTRA FILA DE SAIDA
+            cout << "out :{";
+            while(!this->fila_saida.empty()){// empty = vazio -> nulo.
+                cout << this->fila_saida.front()->id << ":" << this->fila_saida.front()->docs << ":" << this->fila_saida.front()->paciencia << " ";
+                this->fila_saida.pop();
+            }
+            cout << "}" << endl;
     }
-    void mostrar_list(){
-        list<Cliente*>::iterator it;
-        cout << "in: {";
-        for(it = this->fila_entrada.begin(); it != this->fila_entrada.end(); it++){
-            cout << (*it)->id << ":" <<(*it)->docs << ":" << (*it)-> paciencia;
-        }
-        cout << "}" << endl;
-            
-    }
-    void mostrar_queue(){
-        cout << "out: {";
-        while(!this->fila_saida.empty()){// empty = vazio -> nulo.
-            cout << this->fila_saida.front()->id << ":" << this->fila_saida.front()->docs << ":" << this->fila_saida.front()->paciencia;
+
+    void simulacao_banco(int &docs_processados, int &docs_perdidos){
+        while(!this->fila_saida.empty()){
             this->fila_saida.pop();
         }
-        cout << "}" << endl;
-    }
-    void mostrar_fucs(){
-        mostrar_vector();
-        mostrar_list();
-        mostrar_queue();
-    }
-
-    void iniciar_caixas(int quant_caixas){
-     
-    void entrar_cliente(String id, int docs, int paciencia){
-        Cliente *cliente1 = new Cliente(); // com construtor passava por parametro.
-       
-
-        fila_entrada.push_back(cliente1);//add cliente
-    }
-    void mostrar_caixas(){
-        for(i = 0; i < quant; i++){
-            if(caixas[i] == nullptr){
-                
-
-
+        for(int i = 0; i < this->caixas.size(); i++){
+            if(this->caixas[i] != NULL){
+                if(this->caixas[i]->docs > 0){
+                    this->caixas[i]->docs--;
+                    docs_processados++;
+                }else{
+                    this->fila_saida.push(this->caixas[i]);
+                    this->caixas[i] = NULL;
+                }
+            }else{
+                if(!this->fila_entrada.empty()){
+                    this->caixas[i] = this->fila_entrada.front();
+                    this->fila_entrada.pop_front();
+                }
             }
         }
-
-
+        
+        //Diminuindo a paciência
+        list<Cliente*>::iterator it;
+        for(it = this->fila_entrada.begin(); it != this->fila_entrada.end(); ++it){
+            if((*it)->paciencia > 0){
+                (*it)->paciencia--;
+            }
+            //Docs perdidos
+            if((*it)->paciencia == 0 || (*it)->docs == 0){
+                this->fila_saida.push(*it);
+                docs_perdidos = docs_perdidos + (*it)->docs;
+                it = this->fila_entrada.erase(it);
+                it--;
+            }
+        }
     }
-  
-    void passar_tempo(){
-
-        /*para todos os clientes na fila de saida
-            remova cliente do Banco
-        para todos os caixas
-            se existe um Cliente
-                se o cliente tem documentos
-                    processe um documento desse Cliente
-                senao
-                    mova cliente para a fila de saida
-            senao 
-                se hover clientes na fila de entrada
-                    pegue um cliente da fila de entrada
-        para todos os clientes da fila de entrada
-            se paciencia maior que zero
-                decremente um na paciencia
-            senao 
-                ponha na fila*/
+    int aumento_lucro(int &docs_processados, int &quant_caixas){
+        int quant_ideal_caixas = (docs_processados - (quant_caixas * 100));
+        return quant_ideal_caixas;
     }
 };
 
-
 int main(){
+    Banco banco;
+    string opcao;
+    string id;
+    int docs = 0;
+    int paciencia = 0;
+    int quant_caixas = 0;
+    int tempo = 0;
+    int docs_processados = 0;
+    int docs_perdidos = 0;
 
+    while(true){
+        getline(cin, opcao);
+        stringstream in(opcao);
+        in >> opcao;
+
+        if(opcao == "init"){
+            in >> quant_caixas;
+            for(int i = 0; i < quant_caixas; i++){
+                banco.caixas.push_back(NULL);
+            }
+        }else if(opcao == "show"){
+            banco.mostrar_vector();
+        }else if(opcao == "in"){
+            in >> id >> docs >> paciencia;
+            Cliente* elemento = new Cliente(id, docs, paciencia);
+            banco.fila_entrada.push_back(elemento);
+        }else if(opcao == "tic"){
+            banco.simulacao_banco(docs_processados, docs_perdidos);
+            tempo++;
+        }else if(opcao == "finish"){
+            cout << "Recebidos: " << docs_perdidos << endl;
+            cout << "Perdidos: " << docs_perdidos << endl;
+            cout << "Tempo: " << tempo << endl;
+                break;
+            }else{
+                cout << "opcao invalida." << endl;
+            }
+        }
+        int Lucro = banco.aumento_lucro(docs_perdidos, quant_caixas);
+        cout << "Numero de caixas ideal: " << Lucro << " " << " caixas." << endl;
 }
+
